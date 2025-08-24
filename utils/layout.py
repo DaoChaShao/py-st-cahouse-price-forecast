33ef12a1d392a9d323a50c7f73c874b5999b8015
DaoChaShao <27882272@qq.com>
2025-08-24 19:15:19 +0800

    feat: enhance layout.py with page configuration and sidebar navigation setup

--- utils/layout.py
+++ utils/layout.py
@@ -5,3 +5,54 @@
 # @Version  :   Version 0.1.0
 # @File     :   layout.py
 # @Desc     :
+
+from streamlit import set_page_config, Page, navigation
+
+
+def page_config() -> None:
+    """ Set the window
+    :return: None
+    """
+    set_page_config(
+        page_title="AI Customer Support",
+        page_icon=":material/script:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+
+def pages_setter() -> None:
+    """ Set the subpages on the sidebar
+    :return: None
+    """
+    pages: dict = {
+        "page": [
+            "subpages/home.py",
+            "subpages/house.py",
+            "subpages/about.py",
+        ],
+        "title": [
+            "Home",
+            "House Price Prediction",
+            "About",
+        ],
+        "icon": [
+            ":material/home:",
+            ":material/batch_prediction:",
+            ":material/info:",
+        ],
+    }
+
+    structure: dict = {
+        "Introduction": [
+            Page(page=pages["page"][0], title=pages["title"][0], icon=pages["icon"][0]),
+        ],
+        "Core Functions": [
+            Page(page=pages["page"][1], title=pages["title"][1], icon=pages["icon"][1]),
+        ],
+        "Information": [
+            Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
+        ],
+    }
+    pg = navigation(structure, position="sidebar", expanded=True)
+    pg.run()
