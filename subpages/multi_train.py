661b86160231a1258f19d62862e627a1a3313cb7
DaoChaShao <27882272@qq.com>
2025-08-25 01:54:32 +0800

    feat: add multi_train.py with main function template

---
+++ subpages/multi_train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/8/24 19:24
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   multi_train.py
+# @Desc     :   
+
+def main() -> None:
+    """ Main Function """
+    pass
+
+
+if __name__ == "__main__":
+    main()
