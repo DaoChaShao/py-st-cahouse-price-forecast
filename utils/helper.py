f7b2645cbb902706112012d7a482f65b32a1dbf5
DaoChaShao <27882272@qq.com>
2025-08-24 19:15:52 +0800

    feat: add Timer class for measuring code execution time

--- utils/helper.py
+++ utils/helper.py
@@ -5,3 +5,40 @@
 # @Version  :   Version 0.1.0
 # @File     :   helper.py
 # @Desc     :
+
+from time import perf_counter
+
+
+class Timer(object):
+    """ timing code blocks using a context manager """
+
+    def __init__(self, description: str = None, precision: int = 5):
+        """ Initialise the Timer class
+        :param description: the description of a timer
+        :param precision: the number of decimal places to round the elapsed time
+        """
+        self._description: str = description
+        self._precision: int = precision
+        self._start: float = 0.0
+        self._end: float = 0.0
+        self._elapsed: float = 0.0
+
+    def __enter__(self):
+        """ Start the timer """
+        self._start = perf_counter()
+        print()
+        print("-" * 50)
+        print(f"{self._description} has been started.")
+        return self
+
+    def __exit__(self, *args):
+        """ Stop the timer and calculate the elapsed time """
+        self._end = perf_counter()
+        self._elapsed = self._end - self._start
+
+    def __repr__(self):
+        """ Return a string representation of the timer """
+        if self._elapsed != 0.0:
+            print("-" * 50)
+            return f"{self._description} took {self._elapsed:.{self._precision}f} seconds."
+        return f"{self._description} has NOT been started."
