23d9b6d18358b52f2ed272e133a145311cf82673
DaoChaShao <27882272@qq.com>
2025-08-25 16:00:45 +0800

    feat: add slider for variable selection and prediction button in house price prediction settings

--- subpages/multi_predict.py
+++ subpages/multi_predict.py
@@ -6,14 +6,20 @@
 # @File     :   multi_predict.py
 # @Desc     :   
 
-from streamlit import empty, sidebar, subheader, session_state
+from streamlit import (empty, sidebar, subheader, session_state, write,
+                       slider, columns, button, metric)
+
+from utils.helper import Timer
 
 empty_messages: empty = empty()
+left, _ = columns(2, gap="large")
 
 if "data" not in session_state:
     session_state["data"] = None
 if "model_multi" not in session_state:
     session_state["model_multi"] = None
+if "Y_multi" not in session_state:
+    session_state["Y_multi"] = None
 
 with sidebar:
     subheader("House Price Prediction Settings")
@@ -29,4 +35,29 @@ with sidebar:
             empty_messages.info("The model has not been trained yet.")
         else:
             empty_messages.success("The model has been trained with multiple variables.")
-            # Add your prediction code here
+
+            # write(session_state["selected_multi"])
+            for i, col in enumerate(session_state["selected_multi"]):
+                value_min = float(session_state["data"][col].min())
+                value_max = float(session_state["data"][col].max())
+                value_mean = float(session_state["data"][col].mean())
+                step = (value_max - value_min) / len(session_state["data"][col])
+                slider(
+                    f"Select {col}",
+                    min_value=value_min,
+                    max_value=value_max,
+                    value=value_mean,
+                    step=step,
+                    help=f"Select the value of {col} for prediction."
+                )
+
+            if button("Predict House Price", type="primary", use_container_width=True):
+                with Timer("House Price Prediction with Multiple Variables") as timer:
+                    X_multi = session_state["data"][session_state["selected_multi"]].dropna()
+                    y_multi = session_state["model_multi"].predict(X_multi)
+                with left:
+                    metric(
+                        "Predicted House Price", f"$ {y_multi[0]:.2f}",
+                        delta=f"{y_multi[0] - session_state["Y_multi"].mean():.2f}"
+                    )
+                empty_messages.info(timer)
